3da09246582680d065aa003ad2c3e703bc183913
rvadimv <razvodov.vadim@gmail.com>
2024-11-01 20:51:08 +0300

    hws3 week 4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,45 @@
 import {UserType} from '../HW8'
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            /*const sorted = [...state].sort((a, b) => {
+                const nameA = a.name.toLowerCase();
+                const nameB = b.name.toLowerCase();
+
+                if (action.payload === 'up') {
+                    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                } else if (action.payload === 'down') {
+                    return nameA > nameB ? -1 : nameA < nameB ? 1 : 0;
+                }
+                return 0;
+            });
+
+            return sorted;*/
+
+            return [...state].sort((a, b) => {
+                const nameA = a.name.toLowerCase();
+                const nameB = b.name.toLowerCase();
+
+                return action.payload === 'up'
+                    ? nameA.localeCompare(nameB)
+                    : nameB.localeCompare(nameA);
+            });
+
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(n => n.age > 18)
+            // need to fix
         }
         default:
             return state
     }
 }
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
